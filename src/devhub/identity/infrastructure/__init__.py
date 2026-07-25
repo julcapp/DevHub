3ad82f7eacc8b1e853e765d51e5b416fac599985
Александр <julcapp@gmail.com>
2026-07-25 20:44:15 +0700

    feat(identity): export system clock

--- src/devhub/identity/infrastructure/__init__.py
+++ src/devhub/identity/infrastructure/__init__.py
@@ -2,5 +2,6 @@
 
 from devhub.identity.infrastructure.in_memory_provider import InMemoryProvider
 from devhub.identity.infrastructure.sequential_strategy import SequentialStrategy
+from devhub.identity.infrastructure.system_clock import SystemClock
 
-__all__ = ["InMemoryProvider", "SequentialStrategy"]
+__all__ = ["InMemoryProvider", "SequentialStrategy", "SystemClock"]
