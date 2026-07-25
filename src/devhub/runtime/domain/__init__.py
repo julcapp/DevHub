0cc3a34993b40902591e042309ebe5a3ba6b7bad
Александр <julcapp@gmail.com>
2026-07-25 21:14:15 +0700

    feat(runtime): export runtime domain API

---
+++ src/devhub/runtime/domain/__init__.py
@@ -0,0 +1,51 @@
+"""Public Runtime domain vocabulary."""
+
+from devhub.runtime.domain.exceptions import (
+    CapabilityError,
+    CircularDependencyError,
+    DependencyError,
+    DevHubRuntimeError,
+    DiscoveryError,
+    HealthCheckError,
+    LifecycleError,
+    ManifestError,
+    MetadataError,
+    ModuleAlreadyLoadedError,
+    ModuleLoadError,
+    ModuleNotLoadedError,
+)
+from devhub.runtime.domain.models import (
+    Capability,
+    HealthCheck,
+    HealthReport,
+    HealthStatus,
+    LifecycleStage,
+    ModuleMetadata,
+    ModuleMetrics,
+    ModuleState,
+    RuntimeState,
+)
+
+__all__ = [
+    "Capability",
+    "CapabilityError",
+    "CircularDependencyError",
+    "DependencyError",
+    "DevHubRuntimeError",
+    "DiscoveryError",
+    "HealthCheck",
+    "HealthCheckError",
+    "HealthReport",
+    "HealthStatus",
+    "LifecycleError",
+    "LifecycleStage",
+    "ManifestError",
+    "MetadataError",
+    "ModuleAlreadyLoadedError",
+    "ModuleLoadError",
+    "ModuleMetadata",
+    "ModuleMetrics",
+    "ModuleNotLoadedError",
+    "ModuleState",
+    "RuntimeState",
+]
